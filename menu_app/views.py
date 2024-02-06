a64ada1c063a07593c0d840d555b780480bc865c
Administrator <eliseevzv@outlook.com>
2024-02-06 09:09:54 +0500

    fix view for generate urls menu

--- menu_app/views.py
+++ menu_app/views.py
@@ -7,9 +7,10 @@ from menu_app.models import MenuItem
 def index_view(request, menu_name=None):
     if menu_name:
         menu = MenuItem.objects.get(title=menu_name)
-        menus = menu.get_descendants(include_self=True)
+        menus = menu.children.all()
     else:
         menus = MenuItem.objects.all()
 
     current_path = request.path
     return render(request, 'cite/index.html', {'menus': menus, 'current_path': current_path})
+
